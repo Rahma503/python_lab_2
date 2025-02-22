efc8d56ce28a20807d7037c3cf60a36bd7c25f01
Rahma503 <rmdht857@gmail.com>
2025-02-22 22:20:06 +0200

    m2

--- lab2.py
+++ lab2.py
@@ -4,24 +4,24 @@
 # a, b = 10, 5
 # # Expected Output: 15
 
-# op = input("please enter operation add sub div multi : ").lower()
-# n1=int(input("plesae enter num 1 : "))
-# n2=int(input("plesae enter num 2 : "))
-# match op:
-#     case "add" :
-#         print("summtion is ",n1+n2)
-#     case "sub" :
-#         print("subtraction is  ",n1-n2)
-#     case "multi" :
-#         print("multiplication is ",n1*n2)
-#     case "div" :
-#         if n2==0:
-#             print("error can't devide on 0")
-#         else :
+op = input("please enter operation add sub div multi : ").lower()
+n1=int(input("plesae enter num 1 : "))
+n2=int(input("plesae enter num 2 : "))
+match op:
+    case "add" :
+        print("summtion is ",n1+n2)
+    case "sub" :
+        print("subtraction is  ",n1-n2)
+    case "multi" :
+        print("multiplication is ",n1*n2)
+    case "div" :
+        if n2==0:
+            print("error can't devide on 0")
+        else :
 
-#             print("division is ",n1/n2)
-#     case _:
-#         print("invalid input ")
+            print("division is ",n1/n2)
+    case _:
+        print("invalid input ")
 
 # ================================================================================
 
@@ -32,12 +32,12 @@
 # numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 # Expected Output: [1, 3, 5, 7, 9], Count = 5
 
-# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-# res = [num for  num in range(len(numbers)) if num % 2 != 0]  
-# count = len(res)
+res = [num for  num in range(len(numbers)) if num % 2 != 0]  
+count = len(res)
 
-# print(f"{res = }  {count = }")
+print(f"{res = }  {count = }")
 
 
 # ================================================================================
@@ -48,24 +48,25 @@
 # - Contains at least one digit.
 # password = "Pass1234"
 # Expected Output: "Valid Password"
-# password = input("Enter password : ")
 
-# if len(password) >= 8:
-#     flag1=False
-#     flag2=False
-#     for char in password:
-#         if char.isupper():
-#             flag1=True
+password = input("Enter password : ")
+
+if len(password) >= 8:
+    flag1=False
+    flag2=False
+    for char in password:
+        if char.isupper():
+            flag1=True
         
-#         if char.isdigit():
-#             flag2=True
-#     if flag1 and flag2 :
-#         print("Valid password")
-#     else:
-#         print("inValid password")
+        if char.isdigit():
+            flag2=True
+    if flag1 and flag2 :
+        print("Valid password")
+    else:
+        print("inValid password")
 
-# else :
-#     print("inValid password")
+else :
+    print("inValid password")
 
 # ======================================================
 
@@ -77,12 +78,12 @@
 # dic3={5:50,6:60}
 # Expected Result : {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}
 
-# dic1={1:10, 2:20}
-# dic2={3:30, 4:40}
-# dic3={5:50,6:60}
+dic1={1:10, 2:20}
+dic2={3:30, 4:40}
+dic3={5:50,6:60}
 
-# dic4= dic1 | dic2 | dic3
-# print(f"{dic4 = }")
+dic4= dic1 | dic2 | dic3
+print(f"{dic4 = }")
 
 
 # =====================================================
@@ -92,22 +93,27 @@
 # For example, if the string is 'abdulrahman' then the output is:
 # Longest substring in alphabetical order is: abdu
 
-# name = input("enter string : ")
+name = input("enter string : ")
 
-# l = list(name)
+l = list(name)
 
-# longest =current= l[0] 
-# for i in range(1,len(l)):
-#     if l[i] >=l[i-1]:
-#         current+=l[i]
-#     else:
-#         if len(current) > len(longest):
-#             longest=current
-#         current=l[i]
+longest =current= l[0] 
+for i in range(1,len(l)):
+    if l[i] >=l[i-1]:
+        current+=l[i]
+    else:
+        if len(current) > len(longest):
+            longest=current
+        current=l[i]
 
-# print(f"{longest = }")
+print(f"{longest = }")
 
 # ===========================================
+# Question 6:
+# Write a program to check if a Email meets the following criteria:
+# - Ensures the email follows a standard format (e.g., local@domain.com).
+# - Does not check if the email actually exists or is deliverable.
+
 import re 
 email = input("Enter email: ")
 
